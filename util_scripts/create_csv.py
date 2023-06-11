f55196046dee201f3efbfacc442e2e4940a38e86
mert18 <mertuygur02@gmail.com>
2023-06-11 18:57:53 +0300

    fix.

--- util_scripts/create_csv.py
+++ util_scripts/create_csv.py
@@ -1,15 +1,15 @@
 # Script to create CSV data file from Pascal VOC annotation files
-# Based off code from GitHub user datitran: https://github.com/datitran/raccoon_dataset/blob/master/json_to_csv.py
+# Based off code from GitHub user datitran: https://github.com/datitran/raccoon_dataset/blob/master/xml_to_csv.py
 
 import os
 import glob
 import pandas as pd
-import json.etree.ElementTree as ET
+import xml.etree.ElementTree as ET
 
-def json_to_csv(path):
-    json_list = []
-    for json_file in glob.glob(path + '/*.json'):
-        tree = ET.parse(json_file)
+def xml_to_csv(path):
+    xml_list = []
+    for xml_file in glob.glob(path + '/*.xml'):
+        tree = ET.parse(xml_file)
         root = tree.getroot()
         for member in root.findall('object'):
             value = (root.find('filename').text,
@@ -21,16 +21,16 @@ def json_to_csv(path):
                      int(member[1][2].text),
                      int(member[1][3].text)
                      )
-            json_list.append(value)
+            xml_list.append(value)
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
-    json_df = pd.DataFrame(json_list, columns=column_name)
-    return json_df
+    xml_df = pd.DataFrame(xml_list, columns=column_name)
+    return xml_df
 
 def main():
     for folder in ['train','validation']:
         image_path = os.path.join(os.getcwd(), ('images/' + folder))
-        json_df = json_to_csv(image_path)
-        json_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
-        print('Successfully converted json to csv.')
+        xml_df = xml_to_csv(image_path)
+        xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
+        print('Successfully converted xml to csv.')
 
 main()
